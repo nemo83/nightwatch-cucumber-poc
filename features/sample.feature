11772771e8820cbee7a7d29a601ab022d980019a
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2019-02-25 08:37:08 +0000

    saks

--- features/sample.feature
+++ features/sample.feature
@@ -1,21 +1,6 @@
-Feature: As a user, i want to prototype nightwatch, so that I know how to write tests
+Feature: Saks HP
 
-  @smoketest
-  Scenario: Search for an apple cake recipe
-    Given I want to search for an apple cake recipe
-    When I search for "Apple Cake Recipe"
-    Then I see results for an apple cake
-
-  #failing test to show the screenshot in html report
-  Scenario: Verify Page title
-    Given I want to verify the page title text
+  Scenario: Hello world
+    Given I want to verify Saks HP
     When I open the page
-    Then I see the page title is "API Reference | Nightwatch"
-
-  Scenario Outline: Verify Page title
-    Given I want to verify the page title text
-    When I open the page
-    Then I see "<HeadingName>", "<LanguageName>" labels on the page
-    Examples:
-      | HeadingName | LanguageName|
-      | Expect | Language Chains  |
+    Then I check the text is "SAKS.COM BEST SELLERS"
--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,35 +1,17 @@
 const {client} = require('nightwatch-cucumber');
 const {Given, Then, When} = require('cucumber');
 
-Given(/^I want to verify the page title text$/, () => {
-
-    return client.url('http://nightwatchjs.org/api/#assert-visible')
+Given(/^I want to verify Saks HP$/, () => {
+    client.page.SaksPage().navigate().SetVariant()
+    return client.url('https://www.qa.saks.com/Entry.jsp')
 })
+
 When(/^I open the page$/, () => {
-    const testPage = client.page.TestPage()
-    testPage.navigate().waitForElementVisible('@body', 1000)
-})
-Then(/^I see the page title is "([^"]*)"$/, (expectedTitle) => {
-    return client.assert.title(expectedTitle)
+    const saksPage = client.page.SaksPage()
+    saksPage.navigate().waitForElementVisible('@body', 5000)
 })
 
-Then(/^I see "([^"]*)", "([^"]*)" labels on the page$/, function (HeadingName, LanguageName) {
-    const testPage = client.page.TestPage()
-    return testPage.navigate().Fieldlabels(HeadingName, LanguageName)
-});
-
-Given(/^I want to search for an apple cake recipe$/, () => {
-    return client.url('http://www.google.com').pause(2000)
-});
-
-When(/^I search for "([^"]*)"$/, function (RecipeName) {
-    const googleSearchPage = client.page.GoogleSearchPage()
-    googleSearchPage.navigate().setValue('@searchBar', [RecipeName, client.Keys.ENTER])
-
-});
-
-Then(/^I see results for an apple cake$/, () => {
-    client.waitForElementVisible('#resultStats', 1000)
-    return client.assert.elementPresent("#resultStats")
-
-});
+Then(/^I check the text is "([^"]*)"$/, (expectedText) => {
+    client.page.SaksPage().navigate().PrintVisitorId()
+    return client.page.SaksPage().navigate().BestSeller(expectedText)
+})
